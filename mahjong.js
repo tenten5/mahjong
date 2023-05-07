3076488638bcc723e9d6f2bd8f23765b7f390f21
nobuhisa.mizobe <hs4n.mzb@gmail.com>
2023-05-07 11:58:15 +0900

    [add] マンズのテンパイを自動生成して入力デフォルト値にした

--- mahjong.js
+++ mahjong.js
@@ -49,3 +49,56 @@ function isMentsu(tiles, numMentsu) {
   }
   return false;
 }
+
+// 牌姿自動生成
+function generateRandomTiles() {
+  const tiles = new Array(34).fill(0);
+  let total = 9 * 4; 
+  for (let i = 0; i < 13; i++) {
+    let index = Math.floor(Math.random() * total);
+    for (let j = 0; j < 34; j++) {
+      if( index + tiles[j] < 4 ){
+        tiles[j]++;
+        break;
+      }
+      index = index - 4 + tiles[j];
+    }
+    total--;
+  }
+
+  return tiles;
+}
+
+function generateRandomTenpai() {
+  let tiles;
+  do {
+    tiles = generateRandomTiles();
+  } while (calculateWait(tiles).length === 0);
+  return tiles;
+}
+
+// 文字列出力
+function tilesToString(tiles) {
+  let tileString = "";
+  for (let i = 0; i < tiles.length; i++) {
+    for (let j = 0; j < tiles[i]; j++) {
+      const type = Math.floor(i / 9);
+      const num = i % 9 + 1;
+      switch (type) {
+        case 0:
+          tileString += num + "m";
+          break;
+        case 1:
+          tileString += num + "p";
+          break;
+        case 2:
+          tileString += num + "s";
+          break;
+        default:
+          tileString += num + "z";
+          break;
+      }
+    }
+  }
+  return tileString;
+}
